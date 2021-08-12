dd93cb8eb9dc527998ae7585b592bcdbc3ae35ab
awebdevguy <awebdevguy@gmail.com>
2021-08-12 11:27:02 -0600

    removed some songs

---
+++ music/note.md
@@ -0,0 +1,23 @@
+// Music
+// var songs = [
+//   {
+//     name: "jacinto-1",
+//     displayName: "Electric Chill Machine",
+//     artist: "Jacinto Design"
+//   },
+//   {
+//     name: "jacinto-2",
+//     displayName: "Seven Nation Army (Remix)",
+//     artist: "Jacinto Design"
+//   },
+//   {
+//     name: "jacinto-3",
+//     displayName: "Goodnight, Disco Queen",
+//     artist: "Jacinto Design"
+//   },
+//   {
+//     name: "metric-1",
+//     displayName: "Front Row (Remix)",
+//     artist: "Jacinto Design"
+//   }
+// ];
--- script.js
+++ script.js
@@ -10,30 +10,6 @@ var durationEl = document.getElementById("duration");
 const progressContainer = document.getElementById("progress-container");
 var progress = document.getElementById("progress");
 
-// Music
-// var songs = [
-//   {
-//     name: "jacinto-1",
-//     displayName: "Electric Chill Machine",
-//     artist: "Jacinto Design"
-//   },
-//   {
-//     name: "jacinto-2",
-//     displayName: "Seven Nation Army (Remix)",
-//     artist: "Jacinto Design"
-//   },
-//   {
-//     name: "jacinto-3",
-//     displayName: "Goodnight, Disco Queen",
-//     artist: "Jacinto Design"
-//   },
-//   {
-//     name: "metric-1",
-//     displayName: "Front Row (Remix)",
-//     artist: "Jacinto Design"
-//   }
-// ];
-
 var songs = [
   {
     name: "can't you see",
